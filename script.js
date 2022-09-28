37cb8dc4871ca25385137683d8a84e083a225c63
Caleb <calebstem@gmail.com>
2022-09-28 15:14:56 -0400

    Find Winner Function

--- script.js
+++ script.js
@@ -10,28 +10,26 @@ function gameBoardController(arrPos, pawn) {
         gameBoard.turnCount++;
     }
 
-    function checkWinner(){
-        switch(gameBoard.board) {
-            case gameBoard.board[0] == gameBoard.board[1] == gameBoard.board[2]:
-                console.log('Winner');
-                break;
+    let winningPerm = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
+    function checkWinner(boardState){
+        for (let i = 0; i < winningPerm.length; i++){
+           let curPerm = winningPerm[i];
+           if (boardState[curPerm[0]] == boardState[curPerm[1]] && boardState[curPerm[0]] == boardState[curPerm[2]] && boardState[curPerm[0]] !== ''){
+                console.log('Winner')
+           }
         }
     }
 
     if (gameBoard.board[arrPos] == ''){  //checks if cell is empty
-        if (gameBoard.turnCount % 2 == 0) {
+        if (gameBoard.turnCount % 2 == 0) { 
             gameBoardAdd(arrPos, 'x')
         } else {
             gameBoardAdd(arrPos, 'o')
         }
     }
 
-    if (gameBoard.turnCount > 1 && gameBoard.board[0] == gameBoard.board[1] == gameBoard.board[2]){
-        console.log('winner')
-    }
-    
-    return gameBoard.board;
+    checkWinner(gameBoard.board);
 }
 
 function displayController() {
